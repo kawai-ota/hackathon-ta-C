93e264a12da23ae2730d11384f982e3f413d9e60
kohinigeee <seiya47ayies@gmail.com>
2023-07-24 18:49:04 +0900

    [6.9] デザイン変更

--- tutorial/dev13/sample8/src/App.vue
+++ tutorial/dev13/sample8/src/App.vue
@@ -4,3 +4,17 @@
   <div class="style2">Rakus2</div>
   <div class="style3">Rakus3</div>
 </template>
+
+<style>
+.style1 {
+  background-color: #00B7FF;
+}
+
+.style2 {
+  color: #ff0000;
+}
+
+.style3 {
+  font-size: 50px;
+}
+</style>
--- tutorial/dev13/sample9/src/App.vue
+++ tutorial/dev13/sample9/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>Sample9</h1>
-  <button type="button">ボタン1</button>
-  <button type="button">ボタン2</button>
-  <button type="button">ボタン3</button>
+
+  <v-btn>ボタン1</v-btn>
+  <v-btn variant="outline">ボタン2</v-btn>
+  <v-btn variant="tonal" color="orange-darken-3">ボタン３</v-btn>
 </template>
