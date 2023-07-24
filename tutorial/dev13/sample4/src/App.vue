d60d461e3f4db99de9dbd73f68fc496f466cf56a
kohinigeee <seiya47ayies@gmail.com>
2023-07-24 18:43:52 +0900

    [6.7] リアルタイム通信の実装完了

--- tutorial/dev13/sample4/src/App.vue
+++ tutorial/dev13/sample4/src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
 const handleClick = () => {
-  alert("ボタンがクリックされました")
+  alert("ボタンがクリックされました");
+}
+
+const handleMouseOver = () => {
+  alert("ボタンがマウスオーバーされました");
 }
 </script>
 
 <template>
   <h1>Sample4</h1>
   <button @click="handleClick">click</button>
+  <button @mouseover="handleMouseOver">MouserOver</button>
 </template>
 
 
