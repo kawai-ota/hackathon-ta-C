04a378c69824f2e887addae72b2ff36766a52372
FutoshiKoguchi <koguchi.futoshi0126@gmail.com>
2023-07-27 10:45:41 +0900

    [6.9] デザイン変更

--- tutorial/dev12/sample9/src/App.vue
+++ tutorial/dev12/sample9/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Sample9</h1>
-  <button type="button">ボタン1</button>
-  <button type="button">ボタン2</button>
-  <button type="button">ボタン3</button>
+  <v-btn>ボタン1</v-btn>
+  <v-btn variant="outlined">ボタン2</v-btn>
+  <v-btn variant="tonal" color="orange-darken-3">ボタン3</v-btn>
 </template>
